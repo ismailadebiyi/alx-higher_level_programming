361a17684d8ecf9133909f42275b097d5d28b8f1
ismailadebiyi <ismailadebiyi@gmail.com>
2022-11-30 19:24:12 +0100

    add_7

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -2,4 +2,4 @@
 for c in range(ord("a"), ord("z") + 1):
     if (chr(c) != "e"):
         if (chr(c) != "q"):
-            print("{}".format(chr(c)), end="")
+            print("{}".format(chr(c), end="")
