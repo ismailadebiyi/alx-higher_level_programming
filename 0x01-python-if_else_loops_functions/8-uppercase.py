714c0283367ebceb5a68dd29203993c72169bbed
ismailadebiyi <ismailadebiyi@gmail.com>
2022-11-30 19:44:45 +0100

    add_8x

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    cvt = ''
+    for c in str:
+        if ord(c) >= 97:
+            cvt += chr(ord(c) - 32)
+        else:
+            cvt += c
+    print(cvt)
