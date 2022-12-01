94b546ea4bb62d01dcb01229baded7f4bfaaed80
ismailadebiyi <ismailadebiyi@gmail.com>
2022-12-01 20:03:18 +0100

    Fix-0-4

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-from add_0 import add
-a = 1
-b = 2
-sums = add(a, b)
-print("{} + {} = {}".format(a, b, sums))
+if __name__ == "__main__":
+    from add_0 import add
+    a = 1
+    b = 2
+    sums = add(a, b)
+    print("{} + {} = {}".format(a, b, sums))
--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-import sys
-nsum = 0
-for i in range(1, len(sys.argv)):
-    nsum += int(sys.argv[i])
-print("{}".format(nsum))
+if __name__ == "__main__":
+    import sys
+    nsum = 0
+    for i in range(1, len(sys.argv)):
+        nsum += int(sys.argv[i])
+    print("{}".format(nsum))
