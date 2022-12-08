950c7f2cbbba1f859e970475d823d95abb8b8c9a
ismailadebiyi <ismailadebiyi@gmail.com>
2022-12-08 05:07:36 +0100

    lat commit

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -3,7 +3,7 @@ if __name__ == "__main__":
     from sys import argv
     from calculator_1 import add, sub, mul, div
     if len(argv[1:]) != 3:
-        print("Usage:{} <a> <operator> <b>".format(argv[0]))
+        print("{}, Usage:{} <a> <operator> <b>".format(len(argv[1:]), argv[0]))
         exit(1)
     a = int(argv[1])
     b = int(argv[3])
@@ -12,4 +12,4 @@ if __name__ == "__main__":
     if operator not in math:
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
-    print("{} {} {} = {}".format(a, operator, b, math[operator](a, b)))
+    print("{:d} {} {:d} = {:d}".format(a, operator, b, math[operator](a, b)))
