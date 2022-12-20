d3795af9ab4f4126f804b719a0e686d44d81aa30
ismailadebiyi <ismailadebiyi@gmail.com>
2022-12-20 01:35:02 +0100

    add_exception

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+def list_division(my_list_1, my_list_2, list_length):
+    list_div = []
+    for i in range(list_length):
+        try:
+            list_div.append(my_list_1[i] / my_list_2[i])
+            continue
+        except ZeroDivisionError:
+            print("division by 0")
+            list_div.append(0)
+        except TypeError:
+            print("wrong type")
+            list_div.append(0)
+        except IndexError:
+            print("out of range")
+            list_div.append(0)
+        finally:
+            pass
+    return (list_div)
---
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+def raise_exception_msg(message=""):
+    try:
+        print("{}".format(name))
+    except NameError:
+        print(message)
