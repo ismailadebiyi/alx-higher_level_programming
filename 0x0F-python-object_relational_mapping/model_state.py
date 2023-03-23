f46f059dddc24b89a0a22a62c09fdc219ac139e8
ismailadebiyi <ismailadebiyi@gmail.com>
2023-03-23 13:32:04 +0100

    fix_Modal

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""
+Contains State class and Base, an instance of declarative_base()
+"""
+
 from sqlalchemy import Column, Integer, String, MetaData
 from sqlalchemy.ext.declarative import declarative_base
 
