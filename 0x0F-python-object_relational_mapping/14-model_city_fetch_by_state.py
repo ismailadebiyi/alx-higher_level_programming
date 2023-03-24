9f2b8a5852a38c68512d5222ce1103af6016ebc8
ismailadebiyi <ismailadebiyi@gmail.com>
2023-03-24 11:02:32 +0100

    fix_14

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,4 +1,27 @@
 #!/usr/bin/python3
-REPLACE
-return (0)
-print(REPLACE)
+""" view all city in a state """
+
+from model_state import Base, State
+from model_city import City
+from sys import argv, exit
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    if len(argv) != 4:
+        print("Usage: ./14.py <username> <password> <database>")
+        exit(1)
+
+    try:
+        engine = create_engine('mysql://{}:{}@localhost:3306/{}'
+                               .format(argv[1], argv[2], argv[3]))
+    except Exception as err:
+        print(err)
+        exit(1)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    query = session.query(State.name, City.id, City.name)
+                          .filter(State.id == City.state_id)
+    for instance in query:
+        print(instance[0] + ": (" + str(instance[1]) + ") " + instance[2])
--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
 """ city model """
 
-from model_state import Base, State
-from sqlalchemy import Column, Integer, String, ForeignKey, MetaData
+from model_state import Base
+from sqlalchemy import Column, Integer, String, ForeignKy
 from sqlalchemy.ext.declarative import declarative_base
 
-mymetadata = MetaData()
-Base = declarative_base(metadata=mymetadata)
 
-class City(Base, State):
+class City(Base):
     """
     Class with id amd name attribute of city linked to state with foriegn key
     """
